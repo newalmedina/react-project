017eabe4cd969b533acddc859772cd2c5f9612a8
Newal Medina <nmedina@aduxia.com>
2023-02-20 21:20:59 +0100

    corrigiendo posible error de peticiones https

--- app/components/Api/ApiManager.js
+++ app/components/Api/ApiManager.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 
 const ApiManager = axios.create({
-    baseURL: "http://127.0.0.1/react-project/backend-laravel/public/api/v1/"
+    // baseURL: "http://react-project.test/api/v1/",
+    baseURL: "http://127.0.0.1/react-project/backend-laravel/public/api/v1/",
+
 })
 
 
--- app/components/Main.js
+++ app/components/Main.js
@@ -3,19 +3,33 @@ import axios from "axios";
 import Navigation from "./navigation/Navigation";
 import ApiManager from "./Api/ApiManager";
 const Main = () => {
-  const apiUrl = "react-project.test/api/v1";
+
+  const repository = async () => {
+    //   var response = await fetch("http://react-project.test/api/v1/products/get-last-products")
+    var response = await fetch("http://127.0.0.1/react-project/backend-laravel/public/api/v1/products/get-last-products")
+    const json = await response.json()
+    console.log(json)
+  }
 
   useEffect(() => {
+    const config = {
+      headers:
+      {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      }
+    }
+    repository()
 
-    ApiManager.get(apiUrl + '/products/get-last-products')
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.info(error)
-        console.log(error);
-        throw error;
-      });
+    /*  ApiManager.get('products/get-last-products', config)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.info(error)
+          console.log(error);
+          throw error;
+        });*/
   }, []);
   return (
     <>
